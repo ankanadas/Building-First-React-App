ef25148b190ca5d4e9a3a021f990cde3c2cbad3f
ankanadas <aaankyyy@gmail.com>
2021-12-22 09:33:06 +0530

    handling add and delete button

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import Recipe from './Recipe'
 
-export default function RecipeList({recipes}) {
+export default function RecipeList(props) {
+    const {
+        recipes, 
+        handleRecipeAdd, 
+        handleRecipeDelete
+    } = props;
     return (
         <div className='recipe-list'>
             <div>
                 {recipes.map(recipe =>{
                     return (
                         <Recipe 
+                        handleRecipeDelete = {handleRecipeDelete}
                         key={recipe.id} 
                         {...recipe}
                     />)
                 })}
             </div>
             <div className='recipe-list__add-recipe-btn-container'>
-                <button className='btn btn--primary'>Add recipe</button>
+                <button 
+                    className='btn btn--primary'
+                    onClick= {handleRecipeAdd}
+                    >Add recipe</button>
             </div>
         </div>
     )
